e8f369a9a65251486ef2d0a8b1d75af0aea0121f
Marcus Hurney <marcushurney@Marcuss-MacBook-Pro.local>
2017-10-16 15:50:04 -0700

    Adds testimonials content and photos.

--- client/view/Landing/components/Testimonials.js
+++ client/view/Landing/components/Testimonials.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import face1 from '../../../images/face1.jpg';
+import face1 from '../../../images/face1.png';
 import face2 from '../../../images/face2.png';
-import face3 from '../../../images/face3.png';
+import face3 from '../../../images/face3.jpg';
 
 const Testimonials = () => (
   <section className="testimonials_container">
@@ -19,14 +19,14 @@ const Testimonials = () => (
             </div>
             <div className="col s8 m8 l8 quote-col">
               <blockquote>
-                <i className="fa fa-quote-left" />Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                <i className="fa fa-quote-left" />
+                SFRentIndex is like Glassdoor for rental info. I feel so blind
+                looking at rental prices accross the city without it. How do I
+                know if the listings I see are higher or lower than what people
+                are already paying?
               </blockquote>
               <hr className="testimonials-hr" />
-              <cite>&#8212; Mario, successful tree planter</cite>
+              <cite>&#8212; Oliver Townsend</cite>
             </div>
           </div>
         </div>
@@ -38,14 +38,14 @@ const Testimonials = () => (
             </div>
             <div className="col s8 m8 l8 quote-col">
               <blockquote>
-                <i className="fa fa-quote-left" />Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                <i className="fa fa-quote-left" />
+                I used SFRentIndex to lock up my neighbor's rent in the inner
+                sunsent. I quickly realized my neighbors were paying $400 per
+                month less than me. I quickly brought this to my landlord's
+                attention and reduced my rent.
               </blockquote>
               <hr className="testimonials-hr" />
-              <cite>&#8212; Ashley, successful tree planter</cite>
+              <cite>&#8212; Csilla Nemeth</cite>
             </div>
           </div>
         </div>
@@ -57,14 +57,17 @@ const Testimonials = () => (
             </div>
             <div className="col s8 m8 l8 quote-col">
               <blockquote>
-                <i className="fa fa-quote-left" />Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.
+                <i className="fa fa-quote-left" />
+                My landlord raised my rent and justified it as a general market
+                increase. He insisted that my neighbor's rent was also going up,
+                so I asked them what they were paying. My neighbors revealed
+                that their rent was in fact{' '}
+                <span style={{ fontStyle: 'italic' }}>decreasing</span>. That
+                was the inspiration behind SFRentIndex - to empower renters and
+                landlords with accurate rent information.
               </blockquote>
               <hr className="testimonials-hr" />
-              <cite>&#8212; Rebecca, successful tree planter</cite>
+              <cite>&#8212; Varun Barhtia</cite>
             </div>
           </div>
         </div>
