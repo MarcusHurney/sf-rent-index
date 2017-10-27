6ffb13f79cce47fe05a4d4cd09ce6dcc5fc13745
MarcusHurney <marcushurney@Marcuss-MacBook-Pro.local>
2017-10-26 18:36:02 -0700

    Adds new styling and icons to mission section.

--- client/view/Landing/components/Mission.js
+++ client/view/Landing/components/Mission.js
@@ -1,48 +1,49 @@
 import React from 'react';
 
-import orange_hand from '../../../images/orange_hand.svg';
-import grey_neighbors from '../../../images/grey_neighbors.svg';
+import keys from '../../../images/keys.svg';
+import money_house from '../../../images/money_house.svg';
 import navy_map from '../../../images/navy_map.svg';
 
 const Mission = () => (
   <div className="mission_container">
-    <p className="mission_header">Why SF Rent Index?</p>
+    <div className="header_container">
+      <p className="mission_header">Why SF Rent Index?</p>
+    </div>
+
     <div className="columns_container columns">
       <div className="column is-one-third">
-        <figure className="image is-128x128">
-          <img src={orange_hand} alt="orange map icon" />
+        <figure id="keys" className="image is-128x128">
+          <img src={keys} alt="orange map icon" />
         </figure>
         <p className="mission_content">
-          The real estate game in San Francisco is heavily weighted in favor of
-          property owners.
+          The rental market in San Francisco is heavily weighted in favor of
+          property owners. Prospective tenants are expected to silently accept
+          listing prices.
         </p>
       </div>
 
       <div className="column">
-        <figure className="image is-128x128">
-          <img src={grey_neighbors} alt="orange map icon" />
+        <figure id="money_house" className="image is-128x128">
+          <img src={money_house} alt="orange map icon" />
         </figure>
         <p className="mission_content">
-          Without knowledge of what other people in a neighborhood or an
-          apartment are paying, the owner has complete leverage over the tenant.
+          Without knowing what your neighbors are actually paying, the realator
+          has complete leverage over the tenant. Rental prices will always go
+          up, but is this the natural behavior of the real estate economy or a
+          lack of price transparency?
         </p>
       </div>
 
       <div className="column">
-        <figure className="image is-128x128">
+        <figure id="navy_map" className="image is-128x128">
           <img src={navy_map} alt="orange map icon" />
         </figure>
         <p className="mission_content">
-          SF Rent Index is an attempt to make the rental market more transparent
-          without sacraficing the anonimity of tenants.
+          SF Rent Index is an attempt to give the rental market more price
+          transparency without sacraficing the anonimity of its users. Everyone
+          benefits if everyone participates.
         </p>
       </div>
-
-      {/* <div className="column">
-        <figure className="image is-128x128">
-          <img src={navy_map} alt="orange map icon" />
-        </figure>
-      </div> */}
     </div>
   </div>
 );
